4398ae3e2e9b0d5a528c2acda1868067a0f5d06d
junaidirah <junaidirahmat02@gmail.com>
2025-07-25 13:56:09 +0700

    update : DTO create machine, uuid -> manual regex

--- src/feature/pln/dto/create-machine.dto.ts
+++ src/feature/pln/dto/create-machine.dto.ts
@@ -1,21 +1,11 @@
-import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateMachineDto {
-  /**
-   * Pengenal unik untuk mesin, misalnya "Mesin 1", "Unit 3 (3412)", dll.
-   * @example "Mesin 1"
-   * @description Ini adalah identifier yang digunakan untuk membedakan mesin dalam sistem.
-   */
   @IsString()
   @IsNotEmpty()
   identifier: string;
 
-  /**
-   * ID dari Plant (pembangkit) tempat mesin ini berada.
-   * Harus berupa UUID yang valid.
-   * @example "a1b2c3d4-e5f6-7890-1234-567890abcdef"
-   */
-  @IsUUID()
   @IsNotEmpty()
+  @Matches(/^c[a-z0-9]{24}$/) // regex-nya CUID (panjang 25, mulai dengan 'c')
   plantId: string;
 }
